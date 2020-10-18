dd4bd82e7186577466212f303323cedb86eb6e23
snwolyg <569616226@qq.com>
2020-10-18 18:15:09 +0800

    增加搜索 优化分页

--- src/views/article/list.vue
+++ src/views/article/list.vue
@@ -33,6 +33,12 @@
         </template>
       </el-table-column>
 
+      <el-table-column class-name="status-col" label="是否发布" width="110">
+        <template slot-scope="scope">
+          {{ scope.status }}
+        </template>
+      </el-table-column>
+
       <el-table-column width="180px" align="center" label="发布时间">
         <template slot-scope="scope">
           <span>{{ scope.row.display_time | parseTime('{y}-{m}-{d} {h}:{i}:{s}') }}</span>
