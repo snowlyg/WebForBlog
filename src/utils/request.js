91fcf1a94da574ce015c4301853529c304c3e62f
snwolyg <569616226@qq.com>
2020-10-29 16:53:58 +0800

    修复用户搜索报错

--- src/utils/request.js
+++ src/utils/request.js
@@ -46,16 +46,16 @@ service.interceptors.response.use(
     const res = response.data
 
     // if the custom code is not 20000, it is judged as an error.
-    if (res.code !== 200 && res.code !== 401) {
+    if (res.code !== 200) {
+      if (res.code === 401) {
+        this.$router.push('/admin/login')
+      }
+
       Message({
         message: res.message || 'Error',
         type: 'error',
         duration: 5 * 1000
       })
-
-      if (res.code === 401) {
-        this.$router.push('/admin/login')
-      }
 
       // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
       if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
